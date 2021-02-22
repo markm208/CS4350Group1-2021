ae48b7dca375069a6d61211b77d3492a89a72c31
Dustin Osweiler <dosweiler@carthage.edu>
2021-02-22 13:05:24 -0600

    Update math.cpp
    Added function to both divide and multiply the numbers, still need to put into results array

--- math.cpp
+++ math.cpp
@@ -12,10 +12,69 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 //--
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
+    //creates an area to store the decimals in
+    int Mdecimal[];
+
+    //Creates the improper fraction for both numbers
+    int Mfraction1 = (c1 * d1) + n1;
+    int Mfraction2 = (c2 * d2) + n2;
+
+    //Multiplies the two improper fractions into one improper fraction
+    int Mnumerator = Mfraction1 * Mfraction2;
+    int Mdenominator = d1 * d2;
+
+    //Gets the whole number from the fraction and the remainder of the fraction
+    int Mnumber = Mnumerator / Mdenominator;
+    int Mremainder = Mnumerator % Mdenominator;
+
+    //Multiples the remainder until it is larger than the denominator and puts a zero for each time in the array
+    int i = 0;
+    for (int j = 0, j < 10, j++)
+        if (Mremainder < c2) {
+            Mremainder *= 10;
+            Mdecimal[i] = 0;
+            i++;
+        }
+        else {
+            Mdecimal[i] = Mremainder / c2;
+            Mremainder = Mremainder % c2;
+            i++;
+        }
+
     return true;
 }
 //--
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
+    //creates an area to store the decimals in
+    int Ddecimal[];
+
+    //Creates the improper fraction for both numbers
+    int Dfraction1 = (c1 * d1) + n1;
+    int Dfraction2 = (c2 * d2) + n2;
+
+    //Divides the two fractions into one improper fraction
+    int Dnumerator = Dfraction1 * d2;
+    int Ddenominator = Dfraction2 * d1;
+
+    //Gets the whole number from the fraction and the remainder of the fraction
+    int Dnumber = Dnumerator / Ddenominator; 
+    int Dremainder = Dnumerator % Ddenominator;
+
+    //Divides the remainder until it is larger than the denominator and puts a zero for each time in the array
+    int i = 0;
+    for (int j = 0, j < 10, j++)
+        if (Dremainder < c2) {
+            Dremainder *= 10;
+            Ddecimal[i] = 0;
+            i++;
+        }
+        else {
+            Ddecimal[i] = Dremainder / c2;
+            Dremainder = Dremainder % c2;
+            i++;
+        }
+
+
     return true;
 }
