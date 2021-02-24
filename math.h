b5292090a3b6e31a2599b82135f97b4c95b51527
Kai Kuebler <kai.m.kuebler@gmail.com>
2021-02-24 13:49:03 -0600

    added method declarations in math.h

--- math.h
+++ math.h
@@ -6,3 +6,6 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+int findStringLength(char str[]);
+char intToChar(int numToConvert);
+int count_digit(int number);
